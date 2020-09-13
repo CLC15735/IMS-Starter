a3a7c61eacbaceade7f9ffddb65613df4fca907c
Cristina <CLopezCanoyra@qa.com>
2020-09-13 08:41:26 +0100

    Adding items to the .sql files

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NULL DEFAULT NULL,
+    `price` FLOAT NULL DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
