ff86765e5d81e086428138cc88631dd92f003bce
Cristina <CLopezCanoyra@qa.com>
2020-09-14 10:22:17 +0100

    updated the sql files

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
     `item_id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(40) NULL DEFAULT NULL,
+    `category` VARCHAR(40) NULL DEFAULT NULL,
     `price` FLOAT(11) NULL DEFAULT NULL,
     PRIMARY KEY (`item_id`)
 );
